6cc346f29aca87107797a43144bdc233505c55a9
Liz Burke <liz.burke.chi@gmail.com>
2021-01-29 15:27:47 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,25 @@
-# Add your code here
+require 'pry'
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+puts "#{index+1}. #{song} "
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  song_choice = gets.strip
+  end
+
+end
+
+def exit_jukebox
+
+end
